5165a1840b2528d462d89bf58a1537a62f5d836d
fatih6761 <fatihyazici97@hotmail.com>
2014-05-22 01:07:01 +0300

    Initial commit

---
+++ DeterminantCalculator/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeterminantCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nRow, nCol;
+            double[][] matrix;
+
+            Console.WriteLine("Matris Determinant Hesaplama Aracı");
+            Console.WriteLine("2014, Fatih YAZICI");
+            Console.WriteLine("-----------------------------------------------------------");
+
+            Console.WriteLine("KARE matris boyutu (n x n için sadece n girin) :");
+            nRow = nCol = int.Parse(Console.ReadLine());
+
+            matrix = new double[nRow][];
+
+            Console.WriteLine("Matrisi girin :");
+
+            for (int i = 0; i < nRow; i++)
+                matrix[i] = Console.ReadLine()
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => Convert.ToDouble(s))
+                    .ToArray();
+
+            List<int[]> Sigma = VectorPermutations(
+                Enumerable.Range(0, nRow).ToArray());
+
+            double Determinant = 0.0;
+
+            for (int i = 0; i < Sigma.Count; i++)
+            {
+                // sigma permütasyonları ile çarpımların çarpımının toplamı
+                int[] SigmaPerm = Sigma[i];
+                double SigmaSign = Signum(SigmaPerm);
+                double SigmaSum = SigmaSign;
+
+                //debug
+                Console.Write(SigmaSign + " ");
+                foreach (var item in SigmaPerm)
+                    Console.Write((item+1) + " ");
+                Console.WriteLine();
+
+                for (int j = 0; j < SigmaPerm.Length; j++)
+                {
+                    SigmaSum *= matrix[SigmaPerm[j]][j];
+                }
+
+                Determinant += SigmaSum;
+            }
+
+            Console.WriteLine("Determinant : {0}", Determinant);
+
+            Console.ReadLine();
+        }
+
+        private static double Signum(int[] SigmaPerm)
+        {
+            int sgn = 0;
+            for (int i = 0; i < SigmaPerm.Length; i++)
+            {
+                if (SigmaPerm[i] == i)
+                    sgn++;
+            }
+            sgn = SigmaPerm.Length - sgn;
+            if (sgn == 0)
+                return 1.0;
+            else if (sgn % 2 == 0)
+                return -1.0;
+            else
+                return 1.0;
+        }
+
+        static List<int[]> VectorPermutations(int[] vector)
+        {
+            LinkedList<int> elems = new LinkedList<int>(vector);
+
+            return PermutateOrderOfVector(elems, elems.Count).Select(x => x.ToArray()).ToList();
+        }
+
+        static List<LinkedList<int>> PermutateOrderOfVector(LinkedList<int> vector, int a)
+        {
+            if (a < 1)
+            {
+                // Yalnızca son eleman için tek bir permütasyon var
+                // O da gelen dizilimin kendisi
+                return (new LinkedList<int>[] { vector }).ToList();
+            }
+            else
+            {
+                List<LinkedList<int>> sum = new List<LinkedList<int>>();
+                for (int i = 0; i < a; i++)
+                {
+                    // Permüte edilecek vektör
+                    LinkedList<int> A = new LinkedList<int>(vector);
+
+                    // Permütasyon dizisi
+                    int R = A.ElementAt(i);
+                    A.Remove(R);
+
+                    // a-1 elemanlı alt permütasyonları hesapla
+                    List<LinkedList<int>> sPerm = PermutateOrderOfVector(A, a - 1);
+
+                    // Tüm permütasyonların başına R ekle ve sum'da topla
+                    sum.AddRange(sPerm.Select(x => { x.AddFirst(R); return x; }).ToList());
+                }
+                return sum;
+            }
+        }
+    }
+}
---
+++ GaussJordanMethod/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace GaussJordanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // birinci indis satır, ikinci indis sütun
+            int nRow, nCol;
+            double[][] matrix;
+            double[] result;
+
+            Console.WriteLine("Lineer Denklemlerin Gauss-Jordan Yöntemiyle Çözülmesi");
+            Console.WriteLine("2013, Fatih YAZICI");
+            Console.WriteLine("-----------------------------------------------------------");
+
+            Console.WriteLine("Matris boyutlarını girin : ");
+            int[] d = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => int.Parse(s))
+                .ToArray();
+            nRow = d[0]; nCol = d[1];
+
+            matrix = new double[nRow][];
+            result = new double[nCol - 1];
+
+            Console.WriteLine("Matrisi girin :");
+
+            for (int i = 0; i < nRow; i++)
+                matrix[i] = Console.ReadLine()
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => Convert.ToDouble(s))
+                    .ToArray();
+
+            for (int i = 0; i < nRow; i++)
+            {
+                double R = matrix[i][i];
+
+                if (Math.Abs(R) > 0.0)
+                {
+                    matrix[i] = Multiply(matrix[i], 1.0 / R);
+
+                    for (int k = i; k < nRow; k++)
+                    {
+                        double t = matrix[k][i];
+
+                        if (i != k)
+                        {
+                            matrix[k] = Add(matrix[k], Multiply(matrix[i], -t));
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Echelon Matris :");
+            for (int i = 0; i < nRow; i++)
+            {
+                for (int j = 0; j < nCol; j++)
+                {
+                    Console.Write("{0:F2}\t", matrix[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 2; i <= nCol; i++)
+            {
+                double sub = 0.0;
+                for (int j = 2; j < i; j++)
+                {
+                    sub += result[nCol - j] * matrix[nRow - i + 1][nCol - j];
+                }
+                result[nCol - i] = matrix[nRow - i + 1][nCol - 1] - sub;
+            }
+
+            Console.WriteLine("Çözüm :");
+            for (int i = 0; i < nCol - 1; i++)
+            {
+                Console.Write("{0:F2}\t", result[i]);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Replace(double[][] matrix, int row1, int row2)
+        {
+            double[] tmp = matrix[row2];
+            matrix[row2] = matrix[row1];
+            matrix[row1] = tmp;
+        }
+
+        static double[] Multiply(double[] row, double x)
+        {
+            double[] tmp = new double[row.Length];
+            for (int i = 0; i < row.Length; i++)
+            {
+                tmp[i] = row[i] * x;
+            }
+            return tmp;
+        }
+
+        static double[] Add(double[] row1, double[] row2)
+        {
+            double[] tmp = new double[row1.Length];
+            for (int i = 0; i < row1.Length; i++)
+            {
+                tmp[i] = row1[i] + row2[i];
+            }
+            return tmp;
+        }
+    }
+}
